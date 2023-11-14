db5486d9687bfafbb1607a1b7c5e2f17ba591c2d
Aurélien <rofldu08@gmail.com>
2023-11-14 21:04:38 +0100

    Fichier principal javascript

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router'; // Importez votre configuration de routeur
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import './assets/main.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// Utilisez le routeur dans votre application
+app.use(router);
+
+// Montez l'application sur l'élément avec l'ID 'app'
+app.mount('#app');
