d776c52c39db4db2183d0c13d20acf6947ff4f43
Aurélien <rofldu08@gmail.com>
2023-11-28 16:23:47 +0100

    Page à propos du portfolio

---
+++ src/components/pages/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>À Propos</h1>
+    <!-- Contenu de la page About -->
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+/* Styles spécifiques à la page About si nécessaire */
+</style>
