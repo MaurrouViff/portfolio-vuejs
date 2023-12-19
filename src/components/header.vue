0e313d65101adae7a15423a93610338f406ffa0a
Aurélien <rofldu08@gmail.com>
2023-12-19 14:19:51 +0100

    Haut des pages

---
+++ src/components/header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header>
+      <nav class="navbar">
+        <ul class="nav-menu">
+          <li class="nav-item"><router-link to="/accueil" class="link-button">ACCUEIL</router-link></li>
+          <li class="nav-item"><router-link to="/projects" class="link-button">PROJETS</router-link></li>
+          <li class="nav-item"><router-link to="/outils" class="link-button">OUTILS</router-link></li>
+          <li class="nav-item"><router-link to="/contact" class="link-button">CONTACT</router-link></li>
+          <li class="nav-item"><router-link to="/about" class="link-button">À PROPOS</router-link></li>
+        </ul>
+      </nav>
+    </header>
+    <hr class="hr-gradient" style="margin-top: -4px">
+  </div>
+</template>
