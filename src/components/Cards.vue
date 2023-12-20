1956429838caa2efba9a2072c94e4f5304d51981
Aurélien <rofldu08@gmail.com>
2023-12-20 13:20:56 +0100

    Intégration du style des Cards directement dans le composant

---
+++ src/components/Cards.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="main-section">
+    <div class="features">
+      <div class="feature">
+        <div class="feature-title"><img alt="image d'étudiant" class="image-icon" src="../assets/images-icon/student-icon.png">Étudiant</div>
+        <div class="feature-text">Je suis actuellement étudiant en BTS Sio spécialité SLAM.</div>
+      </div>
+      <div class="feature">
+        <div class="feature-title"><img alt="image de développeur" class="image-icon" src="../assets/images-icon/developer-icon.png">Développeur</div>
+        <div class="feature-text">J'apprends certains langages de programmation. Mon rêve est de devenir développeur.</div>
+      </div>
+      <div class="feature">
+        <div class="feature-title"><img src="../assets/images-icon/tools-icon.png" alt="Image d'outils" class="image-icon">Mes projects</div>
+        <div class="feature-text">J'ai fait de nombreux projets, n'hésitez pas à y regarder !</div>
+      </div>
+    </div>
+  </section>
+</template>
