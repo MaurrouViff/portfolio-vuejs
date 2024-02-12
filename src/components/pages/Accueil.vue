122ce2bcf7af6eb13ab8731f524c4fd0c17b1a09
MaurrouViff <rofldu08@gmail.com>
2024-02-12 15:18:48 +0100

    Fichier json pour les outils que j'utilise

--- src/components/pages/Accueil.vue
+++ src/components/pages/Accueil.vue
@@ -1,9 +1,5 @@
 <template>
   <div>
-    <div class="gradient-policy">
-      <h2>Bienvenue ! Je me nomme</h2>
-      <h3>AUBRIET Aurélien</h3>
-    </div>
     <Cards />
 
   </div>
@@ -21,15 +17,6 @@ export default {
 </script>
 
 <style>
-.gradient-policy {
-  font-size: 30px;
-  letter-spacing: -0.72px;
-  line-height: 1.3;
-  background: linear-gradient(45deg, #316aa4 33%, #7B6BB6 50%, #CE6CC9);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  font-weight: 600;
-  text-align: center;
-}
+
 
 </style>
