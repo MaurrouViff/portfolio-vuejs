2b586ab77792310b71b525e599f06bddd2d522cd
MaurrouViff <rofldu08@gmail.com>
2024-02-12 08:36:55 +0100

    Pied de la page

---
+++ src/components/Pied.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <footer>
+      <p>Portfolio fait avec amour par <span>Aurélien</span></p>
+    </footer>
+  </div>
+</template>
+
+<style scoped>
+p {
+  color: black;
+}
+span {
+  color: red;
+}
+</style>
