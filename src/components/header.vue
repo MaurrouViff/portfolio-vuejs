fb0376ba0ded76328ed0e5d1d1c84e7bb59de10d
MaurrouViff <rofldu08@gmail.com>
2024-02-15 08:12:09 +0100

    fichier vue pour le header

--- src/components/header.vue
+++ src/components/header.vue
@@ -3,9 +3,10 @@
     <header>
       <nav class="navbar">
         <ul class="nav-menu">
+          <li class="nav-item"><h1>Portfolio</h1></li>
           <li class="nav-item"><router-link to="/accueil" class="link-button">ACCUEIL</router-link></li>
           <li class="nav-item"><router-link to="/projects" class="link-button">PROJETS</router-link></li>
-          <li class="nav-item"><router-link to="/outils" class="link-button">OUTILS</router-link></li>
+          <li class="nav-item"><router-link to="/outils" class="link-button">LOGICIELS</router-link></li>
           <li class="nav-item"><router-link to="/contact" class="link-button">CONTACT</router-link></li>
         </ul>
       </nav>
@@ -16,18 +17,19 @@
 
 <style scoped>
 .link-button {
-  color: #000000;
+  color: #ffffff;
   padding: 20px 30px;
   border: 1px solid;
   border-radius: 5px;
   cursor: pointer;
   font-size: 16px;
   text-decoration: none;
+  background-color: #186785FF;
   transition: background-color 0.3s, color 0.3s;
 }
 .link-button:hover {
-  background-color: black;
-  color: white;
+  background-color: #007eff;
+  color: #000000;
 }
 .navbar {
   min-height: 12vh;
@@ -35,7 +37,7 @@
   justify-content: center;
   align-items: center;
   padding: 0 24px;
-  background-color: #ffffff;
+  background-color: #000000;
 }
 .nav-menu {
   display: flex;
@@ -57,5 +59,9 @@ header {
   border: 0;
   background-image: linear-gradient(135deg, #006466 24%, #186785 50%, #186785 79%, #316AA4 100%);
   margin-top: -4px
+}
+h1 {
+  color: #ffffff;
+  font-size: 32px;
 }
 </style>
