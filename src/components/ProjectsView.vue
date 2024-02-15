fb76d0274bdeb41767ca52a5a40f201b047dabb4
MaurrouViff <rofldu08@gmail.com>
2024-02-15 08:13:16 +0100

    fichier vue pour les projets

---
+++ src/components/ProjectsView.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+  <section class="main-section">
+    <img src="../assets/images/" alt="Image de score_resto" class="photo">
+
+  </section>
+  <section class="second-section">
+    <img src="../assets/images/" alt="Image Projet roulette PHP" class="photo">
+
+  </section>
+  <section class="third-section">
+    <img src="../assets/images/" alt="Image Projet roulette VueJS" class="photo">
+
+  </section>
+  <section class="fourth-section">
+    <img src="../assets/images/" alt="Image projet compétence" class="photo">
+
+  </section>
+</div>
+</template>
+<script>
+
+</script>
+<style scoped>
+.main-section {
+  min-height: 60vh;
+  padding-top: 126px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 90px;
+  margin-top: -8px;
+}
+.second-section {
+  min-height: 60vh;
+  padding-top: 126px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 90px;
+}
+.third-section {
+  min-height: 60vh;
+  padding-top: 126px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 90px;
+}
+.fourth-section {
+  min-height: 60vh;
+  padding-top: 126px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 90px;
+}
+</style>
