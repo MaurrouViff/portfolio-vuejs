7cf3fa9be2e7775fa97993c1d7386b12d011c727
MaurrouViff <rofldu08@gmail.com>
2024-02-15 08:12:21 +0100

    fichier vue pour les contacts

--- src/components/pages/Contact.vue
+++ src/components/pages/Contact.vue
@@ -1,13 +1,18 @@
-<script setup>
-
-</script>
-
 <template>
 <div>
-
+  <ContactView />
 </div>
 </template>
+<script>
+import ContactView from "../ContactView.vue";
 
+
+export default {
+  components: {
+    ContactView
+  }
+};
+</script>
 <style scoped>
 
 </style>
