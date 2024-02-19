43f380ce1b3ba279701011468630f3575842db90
MaurrouViff <rofldu08@gmail.com>
2024-02-19 16:47:34 +0100

    fichier du cv en ligne

--- src/components/pages/CV.vue
+++ src/components/pages/CV.vue
@@ -1,10 +1,17 @@
 <template>
 <div>
-
+  <CVView />
 </div>
 </template>
 <script>
+import CVView from "../CVView.vue";
 
+
+export default {
+  components: {
+    CVView
+  }
+};
 </script>
 <style>
 
