e01b9727c913f3270260c486cced97d7c6483e5e
MaurrouViff <rofldu08@gmail.com>
2024-02-19 16:52:36 +0100

    fichier du cv en ligne

---
+++ src/components/CVView.vue
@@ -0,0 +1,36 @@
+<template>
+<div id="corps">
+  <section>
+  <div class="gradient-policy">Sous construction</div>
+  </section>
+</div>
+</template>
+
+<style scoped>
+#corps {
+  text-align: center;
+  justify-content: center;
+  justify-items: center;
+}
+.gradient-policy {
+  font-size: 24px;
+  letter-spacing: -0.72px;
+  line-height: 1.3;
+  background: linear-gradient(45deg, #316aa4 33%, #7B6BB6 50%, #CE6CC9);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+
+  font-weight: 600;
+  text-align: center;
+}
+section {
+  min-height: 80vh;
+  padding-top: 126px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-bottom: 90px;
+  margin-top: -8px;
+
+}
+</style>
