fae78684cc2f847a7e7f07864a0036fbda598d19
MaurrouViff <rofldu08@gmail.com>
2024-02-19 17:41:01 +0100

    fichier des projets

--- src/components/ProjectsView.vue
+++ src/components/ProjectsView.vue
@@ -147,17 +147,6 @@ span {
   font-size: 24px;
   margin-bottom: 10px;
 }
-.gradient-policy {
-  font-size: 36px;
-  letter-spacing: -0.72px;
-  line-height: 1.3;
-  background: linear-gradient(45deg, #316aa4 33%, #7B6BB6 50%, #CE6CC9);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-
-  font-weight: 600;
-  text-align: center;
-}
 #font-size {
   font-size: 48px;
 }
