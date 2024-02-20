5a63692f87843de14f04e0d6defd5915b3d5114b
MaurrouViff <rofldu08@gmail.com>
2024-02-20 14:00:13 +0100

    Composent vuejs pour les logiciels

--- src/components/LogicielsView.vue
+++ src/components/LogicielsView.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="corps">
     <h1 id="important">Mes IDE :</h1>
+    <hr />
     <section class="main-section">
       <a :href="getToolLink('PhpStorm')">
         <img src="../assets/images/PhpStorm_Icon.svg.png" alt="Image de PhpStorm" class="photo">
@@ -9,14 +10,7 @@
       <div class="gradient-policy">Cet IDE me permet de travailler sur différents langages comme :</div>
       <div class="gradient-policy">{{ getToolLanguage('PhpStorm') }}</div>
     </section>
-    <section class="second-section">
-      <a :href="getToolLink('PyCharm')">
-        <img src="../assets/images/PyCharm_Icon.svg.png" alt="Image de PyCharm" class="photo">
-      </a>
-      <h1>{{ getToolName('PyCharm') }}</h1>
-      <div class="gradient-policy">Cet IDE me permet de travailler sur différents langages comme :</div>
-      <div class="gradient-policy">{{ getToolLanguage('PyCharm') }}</div>
-    </section>
+    <hr>
     <section class="third-section">
       <a :href="getToolLink('IntelliJ IDEA')">
         <img src="../assets/images/IntelliJ_IDEA_Icon.svg.png" alt="Image d'IntelliJ IDEA" class="photo">
@@ -24,6 +18,15 @@
       <h1>{{ getToolName('IntelliJ IDEA') }}</h1>
       <div class="gradient-policy">Cet IDE me permet de travailler sur différents langages comme :</div>
       <div class="gradient-policy">{{ getToolLanguage('IntelliJ IDEA') }}</div>
+    </section>
+    <hr>
+    <section class="second-section">
+      <a :href="getToolLink('Visual Studio')">
+        <img src="../assets/images/Visual_Studio_Icon_2019.svg.png" alt="Image de Visual Studio" class="photo">
+      </a>
+      <h1>{{ getToolName('Visual Studio') }}</h1>
+      <div class="gradient-policy">Cet IDE me permet de travailler sur différents langages comme :</div>
+      <div class="gradient-policy">{{ getToolLanguage('Visual Studio') }}</div>
     </section>
   </div>
 </template>
@@ -57,6 +60,11 @@ export default {
                 "id": 3,
                 "nom_outils": "PyCharm",
                 "langage": "Python"
+              },
+              {
+                "id": 4,
+                "nom_outils": "Visual Studio",
+                "langage": "C# et C++"
               }
             ]
           }
@@ -79,10 +87,10 @@ export default {
         switch (tool) {
           case 'PhpStorm':
             return 'https://www.jetbrains.com/fr-fr/phpstorm/';
-          case 'PyCharm':
-            return 'https://www.jetbrains.com/fr-fr/pycharm/';
           case 'IntelliJ IDEA':
             return 'https://www.jetbrains.com/fr-fr/idea/';
+          case 'Visual Studio':
+            return 'Lien vers visual studio';
           default:
             return '#';
         }
@@ -100,7 +108,7 @@ export default {
   justify-content: center;
   justify-items: center;
 }
-.main-section, .second-section, .third-section {
+.main-section, .third-section, .second-section {
   min-height: 60vh;
   padding-top: 126px;
   display: flex;
@@ -132,5 +140,10 @@ h1 {
 
   font-weight: 600;
   text-align: center;
+}
+hr {
+  height: 5px;
+  border: 0;
+  background-image: linear-gradient(135deg, #006466 24%, #186785 50%, #186785 79%, #316AA4 100%);
 }
 </style>
