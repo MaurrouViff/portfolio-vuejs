699349e990dfb50d55b1d1bf7c3a2c8f34262093
MaurrouViff <rofldu08@gmail.com>
2024-02-22 08:08:40 +0100

    Composent vuejs pour mes cartes

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,25 +1,25 @@
 <template>
   <div>
-  <section class="main-section">
-    <div class="features">
-      <div class="feature">
-        <div class="feature-title"><img alt="image d'étudiant" class="image-icon" src="../assets/images-icon/student-icon.png">Étudiant</div>
-        <div class="feature-text">Je suis actuellement étudiant en BTS Sio spécialité SLAM !</div>
+    <section class="main-section">
+      <div class="features">
+        <div class="feature">
+          <div class="feature-title"><img alt="image d'étudiant" class="image-icon" src="../assets/images-icon/student-icon.png">Étudiant</div>
+          <div class="feature-text">Je suis actuellement étudiant en BTS Sio spécialité SLAM !</div>
+        </div>
+        <div class="feature">
+          <div class="feature-title"><img alt="image de développeur" class="image-icon" src="../assets/images-icon/developer-icon.png">Développeur</div>
+          <div class="feature-text">J'apprends certains langages de programmation. Mon rêve est de devenir développeur !</div>
+        </div>
+        <div class="feature">
+          <div class="feature-title"><img src="../assets/images-icon/tools-icon.png" alt="Image d'outils" class="image-icon">Mes projects</div>
+          <div class="feature-text">J'ai fait de nombreux projets, n'hésitez pas à y regarder !</div>
+        </div>
+        <div class="feature">
+          <div class="feature-title"><img src="../assets/images-icon/contact-icon.png" alt="Image de téléphone" class="image-icon">Contact</div>
+          <div class="feature-text">N'hésitez pas à me contacter pour avoir des renseignements !</div>
+        </div>
       </div>
-      <div class="feature">
-        <div class="feature-title"><img alt="image de développeur" class="image-icon" src="../assets/images-icon/developer-icon.png">Développeur</div>
-        <div class="feature-text">J'apprends certains langages de programmation. Mon rêve est de devenir développeur !</div>
-      </div>
-      <div class="feature">
-        <div class="feature-title"><img src="../assets/images-icon/tools-icon.png" alt="Image d'outils" class="image-icon">Mes projects</div>
-        <div class="feature-text">J'ai fait de nombreux projets, n'hésitez pas à y regarder !</div>
-      </div>
-      <div class="feature">
-        <div class="feature-title"><img src="../assets/images-icon/contact-icon.png" alt="Image de téléphone" class="image-icon">Contact</div>
-        <div class="feature-text">N'hésitez pas à me contacter pour avoir des renseignements !</div>
-      </div>
-    </div>
-  </section>
+    </section>
   </div>
 </template>
 <style scoped>
