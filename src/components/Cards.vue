8ed6e1ab575aca5116621d37ac0bc9b83ec1adbf
MaurrouViff <rofldu08@gmail.com>
2024-10-02 15:45:45 +0200

    Mise à jour du CV

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -11,7 +11,7 @@
           <div class="feature-text">J'apprends certains langages de programmation. Mon rêve est de devenir développeur !</div>
         </div>
         <div class="feature">
-          <div class="feature-title"><img src="../assets/images-icon/tools-icon.png" alt="Image d'outils" class="image-icon">Mes projects</div>
+          <div class="feature-title"><img src="../assets/images-icon/tools-icon.png" alt="Image d'outils" class="image-icon">Mes projets</div>
           <div class="feature-text">J'ai fait de nombreux projets, n'hésitez pas à y regarder !</div>
         </div>
         <div class="feature">
