2216b67d8fe5f8a24038086ef473eb6b73834510
MaurrouViff <rofldu08@gmail.com>
2024-12-28 18:35:05 +0100

    Ajout du responsive dans la navbar

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,13 @@
   <div class="flex-grow">
   <router-view></router-view>
   </div>
-
+  <Pied/>
 </template>
 
 <script setup>
 
 import Navbar from "@/components/Navbar.vue";
+import Pied from "@/components/Pied.vue";
 </script>
 
 
--- src/pages/Accueil.vue
+++ src/pages/Accueil.vue
@@ -1,9 +1,10 @@
 <script setup>
 
+import Cards from "@/components/Cards.vue";
 </script>
 
 <template>
 <div>
-
+  <Cards/>
 </div>
 </template>
