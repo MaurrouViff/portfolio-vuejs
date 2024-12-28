4373a4aa0f964438efdd3a240a06feef368ce10f
MaurrouViff <rofldu08@gmail.com>
2024-12-28 20:30:30 +0100

    Ajout du responsive dans les cartes

---
+++ src/components/Cards.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <section class="flex items-center justify-center bg-slate-500 min-h-[600px]">
+      <div class="grid grid-cols-1 tablet:gap-4 laptop:gap-5 desktop:gap-6 tablet:grid-cols-2 laptop:grid-cols-2 desktop:grid-cols-3">
+        <!-- Carte 1 -->
+        <div class="bg-white shadow-lg rounded-lg m-2 p-2 tablet:m-3 laptop:m-4 desktop:m-5">
+          <div class="p-3 tablet:p-2 laptop:p-2.5 desktop:p-3">
+            <img src="/contact-img.png" alt="Image de contact">
+            <h3 class="text-lg font-bold text-gray-800 mt-3">Titre de la Carte</h3>
+            <p class="text-gray-600 mt-2">
+              Ceci est un exemple de texte pour une carte. Vous pouvez ajouter une description plus détaillée ici.
+            </p>
+          </div>
+        </div>
+        <!-- Carte 2 -->
+        <div class="bg-white shadow-lg rounded-lg m-2 p-2 tablet:m-3 laptop:m-4 desktop:m-5">
+          <div class="p-3 tablet:p-2 laptop:p-2.5 desktop:p-3">
+            <img src="/contact-img.png" alt="Image de contact">
+            <h3 class="text-lg font-bold text-gray-800 mt-3">Titre de la Carte</h3>
+            <p class="text-gray-600 mt-2">
+              Ceci est un exemple de texte pour une carte. Vous pouvez ajouter une description plus détaillée ici.
+            </p>
+          </div>
+        </div>
+        <!-- Carte 3 -->
+        <div class="bg-white shadow-lg rounded-lg m-2 p-2 tablet:m-3 laptop:m-4 desktop:m-5">
+          <div class="p-3 tablet:p-2 laptop:p-2.5 desktop:p-3">
+            <img src="/contact-img.png" alt="Image de contact">
+            <h3 class="text-lg font-bold text-gray-800 mt-3">Titre de la Carte</h3>
+            <p class="text-gray-600 mt-2">
+              Ceci est un exemple de texte pour une carte. Vous pouvez ajouter une description plus détaillée ici.
+            </p>
+          </div>
+        </div>
+        <!-- Carte 4 -->
+        <div class="bg-white shadow-lg rounded-lg m-2 p-2 tablet:m-3 laptop:m-4 desktop:m-5">
+          <div class="p-3 tablet:p-2 laptop:p-2.5 desktop:p-3">
+            <img src="/contact-img.png" alt="Image de contact">
+            <h3 class="text-lg font-bold text-gray-800 mt-3">Titre de la Carte</h3>
+            <p class="text-gray-600 mt-2">
+              Ceci est un exemple de texte pour une carte. Vous pouvez ajouter une description plus détaillée ici.
+            </p>
+          </div>
+        </div>
+        <!-- Carte 5 -->
+        <div class="bg-white shadow-lg rounded-lg m-2 p-2 tablet:m-3 laptop:m-4 desktop:m-5">
+          <div class="p-3 tablet:p-2 laptop:p-2.5 desktop:p-3">
+            <img src="/contact-img.png" alt="Image de contact">
+            <h3 class="text-lg font-bold text-gray-800 mt-3">Titre de la Carte</h3>
+            <p class="text-gray-600 mt-2">
+              Ceci est un exemple de texte pour une carte. Vous pouvez ajouter une description plus détaillée ici.
+            </p>
+          </div>
+        </div>
+        <!-- Carte 6 -->
+        <div class="bg-white shadow-lg rounded-lg m-2 p-2 tablet:m-3 laptop:m-4 desktop:m-5">
+          <div class="p-3 tablet:p-2 laptop:p-2.5 desktop:p-3">
+            <img src="/contact-img.png" alt="Image de contact">
+            <h3 class="text-lg font-bold text-gray-800 mt-3">Titre de la Carte</h3>
+            <p class="text-gray-600 mt-2">
+              Ceci est un exemple de texte pour une carte. Vous pouvez ajouter une description plus détaillée ici.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
