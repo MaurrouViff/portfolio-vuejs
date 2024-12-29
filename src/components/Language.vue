549b6c46460c336bb307009b2d4622d1890f0526
MaurrouViff <rofldu08@gmail.com>
2024-12-30 00:43:38 +0100

    Ajout des langages & framwork

--- src/components/Language.vue
+++ src/components/Language.vue
@@ -1,7 +1,12 @@
 <template>
 <div>
-  <section class="flex items-center justify-center min-h-[600px] bg-neutral-500">
-    <p class="text-white">TEST</p>
+  <section class="flex items-center justify-center flex-col min-h-[600px] bg-slate-500">
+    <h2 class="font-bold text-red-500 text-base tablet:text-lg laptop:text-xl desktop:text-2xl">Voici les langages que je connais :</h2>
+    <a href="https://www.php.net/"><img src="/php-img.svg" alt="Image de PHP" class="w-32 h-32"></a>
+    <a href="https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference"><img src="/JavaScript-logo.png" alt="Image de JS" class="w-32 h-32"></a>
+    <h2 class="font-bold text-pink-500 text-base tablet:text-lg laptop:text-xl desktop:text-2xl">Voici les frameworks que je connais :</h2>
+    <a href="https://symfony.com/"><img src="/symfony_black_03.png" alt="Image de Symfony" class="w-32 h-32"></a>
+    <a href="https://vuejs.org/"><img src="/Vue.js.svg" alt="Image de Vue.js" class="w-32 h-32"></a>
   </section>
 </div>
 </template>
