b68d9d6581016e918a0f936c89ca12259c2a6aa5
MaurrouViff <rofldu08@gmail.com>
2025-04-21 19:57:04 +0200

    Ajout des routes

--- src/main.js
+++ src/main.js
@@ -5,13 +5,21 @@ import './assets/css/output.css';
 
 // Liste des composants
 import Accueil from './pages/Accueil.vue';
+import Contact from "@/pages/Contact.vue";
+import Langages from "@/pages/Langages.vue";
+import Outils from "@/pages/Outils.vue";
+import About from "@/pages/About.vue";
 
 
 
 // Liste des routes
 const routes = [
     { path: '/', component: Accueil },
-    { path: '/accueil', component: Accueil }
+    { path: '/accueil', component: Accueil },
+    { path: '/contact', component: Contact },
+    { path: '/langages', component: Langages },
+    { path: '/outils', component: Outils },
+    { path: '/a-propos', component: About }
 ];
 
 const router = createRouter({
