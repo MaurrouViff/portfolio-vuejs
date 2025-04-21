2f1479a439d9b0704ae6ae63bc9b9f6357280b49
MaurrouViff <rofldu08@gmail.com>
2025-04-21 18:06:14 +0200

    Ajout de tailwind 4.0

--- src/main.js
+++ src/main.js
@@ -1,21 +1,17 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { createRouter, createWebHistory } from 'vue-router';
+import './assets/css/output.css';
 
 // Liste des composants
 import Accueil from './pages/Accueil.vue';
-import Contact from "@/pages/Contact.vue";
-import Outils from "@/pages/Outils.vue";
-import About from "@/pages/About.vue";
+
 
 
 // Liste des routes
 const routes = [
     { path: '/', component: Accueil },
-    { path: '/accueil', component: Accueil },
-    { path: '/contact', component: Contact },
-    { path: '/outils', component: Outils },
-    { path: '/about', component: About },
+    { path: '/accueil', component: Accueil }
 ];
 
 const router = createRouter({
